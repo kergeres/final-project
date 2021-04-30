59bd7032a8c11a1cccd6b65bae5b87f70f4e54af
kergeres <55786735+kergeres@users.noreply.github.com>
2021-04-30 14:06:44 +0200

    php

---
+++ php/excercises.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>oktatas</title>
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/excercies.css">
+    <link rel="shortcut icon" type="image/svg" href="../img/icons/SVG/bright logo without text.svg"/>
+    <link rel="stylesheet" href="https://use.typekit.net/dcf6lag.css">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu" crossorigin="anonymous">
+
+</head>
+<body onload="loadData()" class="badi">
+    
+    <!-- <i class="fas fa-history"></i>
+    <i class="fas fa-graduation-cap">
+        <i class="fas fa-phone-alt"></i>
+        <i class="fas fa-sign-out-alt"></i> -->
+      <?php include_once ("header.php") ?>
+        <main>
+<div class="content-container badi">
+<h1 class="title">Excercises</h1>
+<div class="exc-container">
+ 
+    <!-- alul a template van. kell -->
+
+<!-- <div class="bar">cs</div>
+<p>Bár a rendelkezésre álló adatok alapján Magyarországon már tetőzött a harmadik hullám, az elmúlt napokban pedig már leszálló ágban van a járvány, hátradőlni még biztosan nem lehet, szerdán is rekordsok halálozást jelentettek a koronavírus miatt, igaz, Müller Cecília ezt később azzal magyarázta, hogy egyfajta adattorlódás történt.</p>
+
+<input class="answerinput" placeholder=" answer"> 
+<button class="excnav excnav-right ">back</button>
+<button class="excnav excnav-left ">next</button> -->
+
+</div>
+
+
+</div> 
+
+
+</main>
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.2.9/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.2.9/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.2.9/firebase-firestore.js"></script>
+
+
+<script src="https://code.responsivevoice.org/responsivevoice.js?key=rAskuAab"></script>
+
+
+<script src="../script/excercises.js"></script>
+<script src="../script/auth.js"></script>
+<script src="../script/main.js"></script>
+    
+    
+</body>
+</html>
